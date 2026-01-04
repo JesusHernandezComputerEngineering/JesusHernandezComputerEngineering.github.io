2bf0e455b7924f430e81e1f8518b55d2e89f7757
Jesus Hernandez <74041096+JesusHernandezComputerEngineering@users.noreply.github.com>
2026-01-03 22:48:23 -0800

    Add files via upload

---
+++ Java/calculator/JFrameExtTest.java
@@ -0,0 +1,28 @@
+public class JFrameExtTest
+
+{
+
+          public static void main (String [] args)
+
+          {
+
+                   //create the JFrameExt object
+
+                   JFrameExt f = new JFrameExt ( );
+
+ 
+
+                   //set frame’s size. (default is 0,0)
+
+                   f.setSize (400, 300);
+
+ 
+
+                   //show the frame (default is noshow)
+
+                   f.setVisible (true);
+
+          }
+
+}
+
